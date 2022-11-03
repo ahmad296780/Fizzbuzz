d7029a3692dd501d6f3a56030a86cbece23109be
ahmad <bha296780@gmail.com>
2022-11-03 11:47:00 -0400

    final

---
+++ Fizzbuzz/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    public class Class1
+    {
+        public string check(int num)
+        {
+            var answer = "";
+            if (num % 3 == 0)
+            {
+                answer = "fizz";
+            }
+            if (num % 5 ==0)
+            {
+                answer += "buzz";
+                    
+            }
+            return answer;
+
+        }
+        
+    }
+}
---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,5 @@
+﻿using Fizzbuzz;
+
+Class1 a = new Class1();
+var b= a.check(3);
+Console.WriteLine(b);
